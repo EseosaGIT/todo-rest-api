54c6d81b96c9df40696410547c5e6e3ab52796d1
Ehioghae Eseosa <eseosaehioghae@gmail.com>
2016-08-02 00:19:45 +0100

    Refactor Todo API with underscore

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var todos = [];
 var todoID = 1;
 
@@ -19,15 +20,10 @@ module.exports = function (app, middleware) {
 
         console.log('all todos'+todos);
         console.log('todoID '+todoID);
-        todos.forEach(function(eachTodo){
-            console.log('todoIDs '+eachTodo.id);
-            if (todoID === eachTodo.id){
-                todoMatch = eachTodo;
-                console.log('todoIDs '+eachTodo.id);
-            }
-        });
+        
+        todoMatch = _.findWhere(todos, {id: todoID});
 
-        if (todoMatch === null){ // meaning it wasnt found
+        if (todoMatch === null || todoMatch === undefined){ // meaning it wasnt found
             res.status(404).send('Todo not found');
         } else { // it was found
             res.json(todoMatch); // so send the json format of the found object
@@ -40,7 +36,14 @@ module.exports = function (app, middleware) {
     // POST /todos  
     app.post('/todos', middleware.jsonParser, function (req, res) {
         var jsonBody = req.body;
+
+        jsonBody = _.pick(jsonBody, 'description', 'completed');
+
+        if(!_.isBoolean(jsonBody.completed) || !_.isString(jsonBody.description) || jsonBody.description.trim().length === 0)
+            return res.status(400).send('Wrong values POSTed');
+
         jsonBody.id = todoID++; // incrementing the todoID and storing it in the current POSTed todo
+        jsonBody.description = jsonBody.description.trim();
 
         todos.push(jsonBody);
         res.send(todos);
