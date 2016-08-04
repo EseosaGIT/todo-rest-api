054a923be180678e40ed7939245579fa058edd47
Ehioghae Eseosa <eseosaehioghae@gmail.com>
2016-08-04 20:56:19 +0100

    Add postgres on heroku

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var sequelize;
 
 if (env === 'production') { // environment variable for production
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
+        'dialect': 'postgres'
     })
 } else {
         sequelize = new Sequelize(undefined, undefined, undefined, {
